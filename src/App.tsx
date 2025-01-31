a01cd08825f7cebd667a3a1b7abb0718660a7ed0
alexkolitenkov <alex.kolitenkov@gmail.com>
2025-01-31 11:24:01 +0200

    fix: removed duplicate provider store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import MainPage from './pages/main/Main';
 
 const App = () => {
   return (
-    <Provider store={store}>
       <MainPage />
-    </Provider>
   );
 };
 
